f0704cb47ece042a648b0e9c4d2034d0b12b2212
Sergio <kukrin>
2022-05-23 02:36:23 -0300

    se agrega info

---
+++ logica/logica.js
@@ -0,0 +1,28 @@
+function precio() {
+    
+    let tipo = document.getElementById("tipoticket").value;
+    let cantidad = document.getElementById("cantidad").value;
+    let valor = cantidad * 200;
+    
+    if(tipo == "Estudiante"){
+        valor= valor * 0.2;
+    }
+    else if( tipo == "Trainee"){
+        valor= valor * 0.5;
+    }
+    else{
+        valor = valor * 0.85;
+    }
+    
+    document.getElementById("precio").innerHTML=" Total a Pagar: $" + valor;
+    }
+    
+    let resumen= document.getElementById("resumen");
+    resumen.onclick = precio;
+
+    const borrar = () => {
+        document.getElementById('cantidad').value = null
+        document.getElementById('tipoticket').value = null
+        document.getElementById('precio').innerText = null
+        cantidad, tipoticket, precio = 0
+      }
